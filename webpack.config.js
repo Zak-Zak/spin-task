af2e5c7c7b39d5002cc39bf9bc52cc8f203b89d9
Zakvan Gainullin <zakvan94@gmail.com>
2022-02-23 21:25:51 +0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const NodePolyfillPlugin = require("node-polyfill-webpack-plugin");
+const NODE_ENV = process.env.NODE_ENV;
+
+module.exports = {
+    mode: NODE_ENV || 'development',
+    entry: path.resolve(__dirname, './src/index.ts'),
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '.json'],
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: path.resolve(__dirname, 'public/index.html'),
+        }),
+        new NodePolyfillPlugin(),
+    ],
+    devServer: {
+        port: 3000,
+        open: false,
+        hot: true,
+    },
+};
